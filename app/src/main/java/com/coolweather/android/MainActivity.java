7caf62c8acfc8749d77ce0fcd67af7f1b12f5208
xiaozeng26 <1254520806@qq.com>
2020-06-17 22:28:40 +0800

    update

--- app/src/main/java/com/coolweather/android/MainActivity.java
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -2,6 +2,7 @@ package com.coolweather.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
